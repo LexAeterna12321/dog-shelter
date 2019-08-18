685cff6049c6395479ae52a84dada15093e20ad0
Lex Aeterna <lexaeterna12321@gmail.com>
2019-08-18 22:28:52 +0200

    Updates

---
+++ precache-manifest.b7f6323d714b8f50b5f01a57d9f9e9b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03f873ff7778b25f6a849beca7786c5",
+    "url": "/dog-shelter/index.html"
+  },
+  {
+    "revision": "6072811889a76a4acc79",
+    "url": "/dog-shelter/static/css/main.b38a3de4.chunk.css"
+  },
+  {
+    "revision": "54cf56b639a09743f8fd",
+    "url": "/dog-shelter/static/js/2.b13f12d5.chunk.js"
+  },
+  {
+    "revision": "6072811889a76a4acc79",
+    "url": "/dog-shelter/static/js/main.2088a333.chunk.js"
+  },
+  {
+    "revision": "d14a908901eab67bec54",
+    "url": "/dog-shelter/static/js/runtime~main.07898519.js"
+  },
+  {
+    "revision": "4eb85e80248f3371f721a9687e16d075",
+    "url": "/dog-shelter/static/media/hazel.4eb85e80.jpg"
+  },
+  {
+    "revision": "2f47cb659ae081c9e07bc25c264f0aed",
+    "url": "/dog-shelter/static/media/tubby.2f47cb65.jpg"
+  },
+  {
+    "revision": "5c1e3b17efc4e2197352567ea0e262c1",
+    "url": "/dog-shelter/static/media/whiskey.5c1e3b17.jpg"
+  }
+]);
